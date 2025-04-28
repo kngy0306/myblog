cfcb34006d1810469492c0ed2d8b86407bca95d3
kona <kitami.kitano20@gmail.com>
2025-04-28 10:46:19 +0900

    :memo: MDXに対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     service: passthroughImageService(),
   },
+
+  integrations: [mdx()],
 });
