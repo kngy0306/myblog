1db173ee475b958747ea474fb8bdaf2b17c8a837
kona <kitami.kitano20@gmail.com>
2025-05-01 23:12:21 +0900

    :art: Markdownプラグインとして、改行を<br>に変換するプラグインを追加
    https://docs.astro.build/ja/guides/markdown-content/#markdown%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import mdx from "@astrojs/mdx";
+import remarkBreaks from "remark-breaks";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+  },
   image: {
     service: passthroughImageService(),
   },
